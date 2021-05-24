c174797857f00739286c11dfdc05145f9a9313a3
yichimaiqun <13068944359@163.com>
2021-05-24 15:55:17 +0800

    代码通用化改造

--- src/main/java/com/yichimai/excel/utils/ExcelToDatabaseUtil.java
+++ src/main/java/com/yichimai/excel/utils/ExcelToDatabaseUtil.java
@@ -5,26 +5,31 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cn.hutool.core.lang.Console;
+import cn.hutool.extra.pinyin.PinyinUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.sax.Excel07SaxReader;
 import cn.hutool.poi.excel.sax.handler.RowHandler;
 
+/**
+ * 把excel文件导入数据库
+ *
+ */
 public class ExcelToDatabaseUtil {
 
 	
 	public static void main(String[] args) {
-		Excel07SaxReader reader = new Excel07SaxReader(createRowHandler());
-		reader.read("d:/1008/test.xlsx", 0);
+//		Excel07SaxReader reader = new Excel07SaxReader(createRowHandler());
+//		reader.read("d:/1008/test.xlsx", 0);
 		
-		
-//		ExcelUtil.readBySax("D:\\1008\\test.xlsx", 0, createRowHandler());
 		
 	}
-	private static String INSERT_SQL = "";
+	
 	private static PreparedStatement PS = null;
 	
 	private static RowHandler createRowHandler() {
@@ -35,8 +40,8 @@ public class ExcelToDatabaseUtil {
 	            if(rowIndex == 0) {
 //	            	System.out.println(rowlist.size());
 	            	try {
-						createTable(rowlist.size());
-						createInsertSql(rowlist.size());
+	            		createTableAndInsertSql(rowlist);
+//						createInsertSql(rowlist.size());
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -57,37 +62,102 @@ public class ExcelToDatabaseUtil {
 	    };
 	}
 	
+	private static final String URL = "jdbc:mysql://localhost:3306/dbf?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC";
+	private static final String USERNAME = "root";
+	private static final String PASS = "root";
 	
-	private static void createInsertSql(int cowSize) throws Exception {
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbf?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC", "root", "root");
+	
+	private static void createTableAndInsertSql(List<Object> rowlist) throws Exception {
 		
-		/**
-		 * create the insert sql statement
-		 */
-		StringBuilder sb = new StringBuilder();
-		sb.append("insert into testExcel" );
-		sb.append("(");
-		for(int i = 0; i<cowSize ; i++) {
-			sb.append("c" + i +  ",");
+		Connection conn = DriverManager.getConnection(URL, USERNAME, PASS);
+		Statement stat = conn.createStatement();
+		
+		StringBuilder createTableSB = new StringBuilder();
+		createTableSB.append("create table testExcel");
+		createTableSB.append("(");
+		createTableSB.append("id int primary key AUTO_INCREMENT");  // first title must be id, auto increment
+
+		OLD_COLUMN_NAME_COUNT_MAP = new HashMap<>();//重置一下
+		StringBuilder insertSB = new StringBuilder();
+		insertSB.append("insert into testExcel(");
+		for(int i = 0; i<rowlist.size() ; i++) {
+			
+			String currentCellString = rowlist.get(i).toString().trim();
+			String py = PinyinUtil.getFirstLetter(currentCellString, "");//获取拼音首字母，作为字段候选
+			
+			//确定了字段拼写方式后，需要对字段进行去重操作
+			py = unrepeatColumnName(py);
+			
+			createTableSB.append(", " + py + " varchar(400)");//建表语句加字段
+			
+			insertSB.append( py +  ",");//insert语句加字段
 		}
-		sb.setLength(sb.length()-1);//  remove the comma at the end
-		sb.append(")");
-		sb.append("values(");
-		for(int i = 0; i<cowSize ; i++) {
-			sb.append("?,");
+		insertSB.setLength(insertSB.length()-1);//  remove the comma at the end
+		insertSB.append(") values(");
+		for(int i = 0; i<rowlist.size() ; i++) {
+			insertSB.append("?,");
 		}
-		sb.setLength(sb.length()-1);//  remove the comma at the end
-		sb.append(")");
+		insertSB.setLength(insertSB.length()-1);//  remove the comma at the end
+		insertSB.append(")");
+		PS = conn.prepareStatement(insertSB.toString());
 		
-		INSERT_SQL = sb.toString();
-		PS = conn.prepareStatement(INSERT_SQL);
+		createTableSB.append(")");
 		
+		stat.executeUpdate(createTableSB.toString());
+		stat.close();
+		conn.close();
+	}
+	
+	private static Map<String,Integer> OLD_COLUMN_NAME_COUNT_MAP = new HashMap<>();
+	//字段名称去重（后面加上数字的方式）
+	private static String unrepeatColumnName(String originName) {
+		if(OLD_COLUMN_NAME_COUNT_MAP.containsKey(originName)) {
+			Integer count = OLD_COLUMN_NAME_COUNT_MAP.get(originName);
+			count++;
+			OLD_COLUMN_NAME_COUNT_MAP.put(originName, count);
+			return originName + count;
+		}else {
+			OLD_COLUMN_NAME_COUNT_MAP.put(originName, 1);
+			return originName;
+		}
 	}
 	
 	
-	private static void createTable(int cowSize) throws Exception {
+//	private static void createInsertSql(int cowSize) throws Exception {
+//		Connection conn = DriverManager.getConnection(URL, USERNAME, PASS);
+//		/**
+//		 * create the insert sql statement
+//		 */
+//		StringBuilder sb = new StringBuilder();
+//		sb.append("insert into testExcel" );
+//		sb.append("(");
+//		for(int i = 0; i<cowSize ; i++) {
+//			sb.append("c" + i +  ",");
+//		}
+//		sb.setLength(sb.length()-1);//  remove the comma at the end
+//		sb.append(")");
+//		sb.append("values(");
+//		for(int i = 0; i<cowSize ; i++) {
+//			sb.append("?,");
+//		}
+//		sb.setLength(sb.length()-1);//  remove the comma at the end
+//		sb.append(")");
+//		
+//		INSERT_SQL = sb.toString();
+//		PS = conn.prepareStatement(INSERT_SQL);
+//		
+//	}
+	
+	
+	
+	/**
+	 * 废弃的方法
+	 * @param cowSize
+	 * @throws Exception
+	 */
+	private static void createTable2(int cowSize) throws Exception {
 		
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbf?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC", "root", "root");
+		Connection conn = DriverManager.getConnection(URL, USERNAME, PASS);
 		Statement stat = conn.createStatement();
 		
 		StringBuilder sb = new StringBuilder();
@@ -104,7 +174,6 @@ public class ExcelToDatabaseUtil {
 		stat.executeUpdate(sb.toString());
 		stat.close();
 		conn.close();
-		
 	}
 	
 }
