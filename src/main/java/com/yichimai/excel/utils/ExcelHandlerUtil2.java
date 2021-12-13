b0808394f8c02bc968db2c92a438983f5a6dd3b3
yichimaiqun <13068944359@163.com>
2021-12-13 14:07:49 +0800

    增加了研究生条码业务功能

--- src/main/java/com/yichimai/excel/utils/ExcelHandlerUtil2.java
+++ src/main/java/com/yichimai/excel/utils/ExcelHandlerUtil2.java
@@ -47,6 +47,8 @@ import com.yichimai.excel.controller.DbfToDatabaseController;
  * 
  * 目前已经涵盖的考试：
  * 教师资格证考试
+ * 学业水平考试（院校的index不同）
+ * 研究生考试：虽然多了“单元划分”，但是仅凭科目即可保证唯一，因此同样使用科目进行划分（2021-12-09）
  *
  */
 public class ExcelHandlerUtil2 {
@@ -89,7 +91,7 @@ public class ExcelHandlerUtil2 {
         
         Footer newFooter = newSheet.getFooter();
         newFooter.setCenter(HSSFFooter.page()+" / "+HSSFFooter.numPages());
-        newFooter.setLeft("禁止先勾再检查，禁止连续勾几行");
+        newFooter.setLeft("禁止先勾后检查，禁止连续勾几行");
         newFooter.setRight("技术支持：xiaojz");
         
         handle_split_by_school(examType, wb, sheetIndex, schoolCodeCellIndex, pageSize, newWorkbook, newSheet, newCellStyle);
@@ -141,36 +143,91 @@ public class ExcelHandlerUtil2 {
 		case "jszg":
 			handle_school_jszgz(  sheetIndex, schoolCodeCellIndex, schoolListCache, pageSize, newSheet,newCellStyle);
 			break;
+			
+		case "sk":
+			handle_school_sk(  sheetIndex, schoolCodeCellIndex, schoolListCache, pageSize, newSheet,newCellStyle);
+			break;
 
+		case "sy":
+			handle_school_sy(  sheetIndex, schoolCodeCellIndex, schoolListCache, pageSize, newSheet,newCellStyle);
+			break;
 		default:
             throw new Exception("examType error");
 		}
+	}
+	
+	private static void handle_school_sk(int sheetIndex,int schoolCodeCellIndex,
+		 List<Row> schoolListCache,int pageSize,
+		SXSSFSheet newSheet,CellStyle newCellStyle) throws Exception {
+		int columnInColumnSize = 5; //在新版式里面的一个大列里面有多少小列
+		int columnSize = 3; //在新版式里面有 多少个大列
+		int subjectCodeIndex = 6;
+		int subjectNameIndex = 7;
+		boolean showSubjectName = true; //是否打印科目名称
+		int classIndex = 8;
+		int firstNameIndex = 9;
+		int lastNameIndex = 10;
 		
+		handle_school_common(sheetIndex, schoolCodeCellIndex, schoolListCache, pageSize, newSheet, newCellStyle, columnInColumnSize, columnSize, subjectCodeIndex, subjectNameIndex, showSubjectName, classIndex, firstNameIndex, lastNameIndex, "科目");
+	}
+	
+	//硕士研究生考试
+	private static void handle_school_sy(int sheetIndex,int schoolCodeCellIndex,
+			 List<Row> schoolListCache,int pageSize,
+			SXSSFSheet newSheet,CellStyle newCellStyle) throws Exception {
+			int columnInColumnSize = 5; //在新版式里面的一个大列里面有多少小列
+			int columnSize = 3; //在新版式里面有 多少个大列
+			int subjectCodeIndex = 4;
+			int subjectNameIndex = 5;
+			boolean showSubjectName = false; //是否打印科目名称
+			int classIndex = 5;
+			int firstNameIndex = 7;
+			int lastNameIndex = 8;
+			
+			handle_school_common(sheetIndex, schoolCodeCellIndex, schoolListCache, pageSize, newSheet, newCellStyle, columnInColumnSize, columnSize, subjectCodeIndex, subjectNameIndex, showSubjectName, classIndex, firstNameIndex, lastNameIndex, "单元");
+		}
+	
+	
+	private static void handle_school_jszgz(int sheetIndex,int schoolCodeCellIndex,
+			 List<Row> schoolListCache,int pageSize,
+				SXSSFSheet newSheet,CellStyle newCellStyle) throws Exception {
+		int columnInColumnSize = 5; //在新版式里面的一个大列里面有多少小列
+		int columnSize = 3; //在新版式里面有 多少个大列
+		int subjectCodeIndex = 5;
+		int subjectNameIndex = 6;
+		boolean showSubjectName = false; //是否打印科目名称
+		int classIndex = 7;
+		int firstNameIndex = 8;
+		int lastNameIndex = 9;
+		
+		handle_school_common(sheetIndex, schoolCodeCellIndex, schoolListCache, pageSize, newSheet, newCellStyle, columnInColumnSize, columnSize, subjectCodeIndex, subjectNameIndex, showSubjectName, classIndex, firstNameIndex, lastNameIndex, "科目");
 	}
 	
 	
-	
-	private static final int JSZG_COLUMN_IN_COLUMN_SIZE = 5;//教师资格在新版式里面的一个大列里面有多少小列
-	private static final int JSZG_COLUMN_SIZE = 3;//教师资格在新版式里面有 多少个大列
-	
-	//教师资格证 右侧列数：5
-	//表头字段：省份代码	考区代码	考区名称	考点代码	考点名称
-	//表格字段：科目代码	科目名称	考场号	起始姓名	结束姓名
-	private static void handle_school_jszgz(int sheetIndex,int schoolCodeCellIndex,
+	/**
+	 * 抽取公共办法
+	 * @param columnInColumnSize  在新版式里面的一个大列里面有多少小列
+	 * @param columnSize 在新版式里面有 多少个大列
+	 * 2021-12-10 有些不一定使用科目，所以使用subjectName自定义
+	 */
+	private static void handle_school_common(int sheetIndex,int schoolCodeCellIndex,
 			 List<Row> schoolListCache,int pageSize,
-			SXSSFSheet newSheet,CellStyle newCellStyle) throws Exception {
+			SXSSFSheet newSheet,CellStyle newCellStyle,
+			int columnInColumnSize,int columnSize,
+			int subjectCodeIndex, int subjectNameIndex,boolean showSubjectName,
+			int classIndex,int firstNameIndex,int lastNameIndex,String subjectName) throws Exception {
 		//设置好列宽
 		newSheet.setDefaultColumnWidth(7*256);//设置7个字符的宽度（7个数字）
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*0 + 0, 5*256);//第一列
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*1 + 0, 5*256);
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*2 + 0, 5*256);
+		newSheet.setColumnWidth(columnInColumnSize*0 + 0, 5*256);//第一列
+		newSheet.setColumnWidth(columnInColumnSize*1 + 0, 5*256);
+		newSheet.setColumnWidth(columnInColumnSize*2 + 0, 5*256);
 
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*0 + 3, 5*256);//“检查”列
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*1 + 3, 5*256);
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*2 + 3, 5*256);
+		newSheet.setColumnWidth(columnInColumnSize*0 + 3, 5*256);//“检查”列
+		newSheet.setColumnWidth(columnInColumnSize*1 + 3, 5*256);
+		newSheet.setColumnWidth(columnInColumnSize*2 + 3, 5*256);
 
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*0 + 4, 2*256);//空白列
-		newSheet.setColumnWidth(JSZG_COLUMN_IN_COLUMN_SIZE*1 + 4, 2*256);
+		newSheet.setColumnWidth(columnInColumnSize*0 + 4, 2*256);//空白列
+		newSheet.setColumnWidth(columnInColumnSize*1 + 4, 2*256);
 		
 		/**
 		 * 数据的预处理，先把数据加工好，再去填充新sheet
@@ -180,20 +237,21 @@ public class ExcelHandlerUtil2 {
 		String subjectCodeCache = null;//教师资格证科目代码的index是5，标志位判断科目是否发生变化
 		StringBuilder sb = new StringBuilder();
 		for(Row rowOfSchool : schoolListCache) {
-			String currentSubjectCode = rowOfSchool.getCell(5).getStringCellValue();//科目代码的index是5
+			String currentSubjectCode = rowOfSchool.getCell(subjectCodeIndex).getStringCellValue();
+			String currentSubjectName = showSubjectName? "-" + rowOfSchool.getCell(subjectNameIndex).getStringCellValue():"";
 			if(!currentSubjectCode.equals(subjectCodeCache)) {
 				subjectCodeCache = currentSubjectCode;
 				//插入空行、科目行、标题行
 				targetStrList.add("");
-				targetStrList.add("科目-" + currentSubjectCode);
+				targetStrList.add(subjectName + "-" + currentSubjectCode + currentSubjectName);
 				targetStrList.add("考场-起始姓名-结束姓名-检查");
 			}
 			sb.setLength(0);// clear
-			sb.append(rowOfSchool.getCell(7).getStringCellValue().toString());//考场号
+			sb.append(rowOfSchool.getCell(classIndex).getStringCellValue().toString());//考场号
 			sb.append("-");
-			sb.append(rowOfSchool.getCell(8).getStringCellValue().toString());//姓名
+			sb.append(rowOfSchool.getCell(firstNameIndex).getStringCellValue().toString());//姓名
 			sb.append("-");
-			sb.append(rowOfSchool.getCell(9).getStringCellValue().toString());//姓名
+			sb.append(rowOfSchool.getCell(lastNameIndex).getStringCellValue().toString());//姓名
 			sb.append("- ");// add an extra space, make last cell have border
 			targetStrList.add(sb.toString());
 		}
@@ -206,15 +264,15 @@ public class ExcelHandlerUtil2 {
 		int columnIndexOfPageList = 0;//记录已经写到第几列 0开始
 		boolean evenPage = false;//判断是否偶数页码？不是就需要补充一页空白
 
-		createNewPage(pageSize, schoolCodeCellIndex, newSheet, pageList, schoolListCache, JSZG_COLUMN_IN_COLUMN_SIZE,evenPage);
+		createNewPage(pageSize, schoolCodeCellIndex, newSheet, pageList, schoolListCache, columnInColumnSize,evenPage);
 		for(String targetStr : targetStrList) {
 			if(targetStr.length() == 0) {
 				//空白行
 			}else {
 				String[] split = targetStr.split("-");
 				for(int index = 0;index < split.length; index++) {
-					pageList.get(rowIndexOfPageList).getCell(columnIndexOfPageList*JSZG_COLUMN_IN_COLUMN_SIZE + index).setCellValue(split[index]);
-					pageList.get(rowIndexOfPageList).getCell(columnIndexOfPageList*JSZG_COLUMN_IN_COLUMN_SIZE + index).setCellStyle(newCellStyle);
+					pageList.get(rowIndexOfPageList).getCell(columnIndexOfPageList*columnInColumnSize + index).setCellValue(split[index]);
+					pageList.get(rowIndexOfPageList).getCell(columnIndexOfPageList*columnInColumnSize + index).setCellStyle(newCellStyle);
 				}
 			}
 			
@@ -222,20 +280,21 @@ public class ExcelHandlerUtil2 {
 			if(rowIndexOfPageList == pageSize) {
 				rowIndexOfPageList = 1;//重新开始的1行
 				columnIndexOfPageList++;
-				if(columnIndexOfPageList == JSZG_COLUMN_SIZE) {
+				if(columnIndexOfPageList == columnSize) {
 					columnIndexOfPageList = 0;//重新开始的1页，从0列开始
 					//创建新开始的1页
 					evenPage = !evenPage;//标记是否偶数页
-					createNewPage(pageSize, schoolCodeCellIndex, newSheet, pageList, schoolListCache, JSZG_COLUMN_IN_COLUMN_SIZE,evenPage);
+					createNewPage(pageSize, schoolCodeCellIndex, newSheet, pageList, schoolListCache, columnInColumnSize,evenPage);
 				}
 			}
 		}
 		if(!evenPage) {//不是偶数页
 			evenPage = !evenPage;//标记是否偶数页
-			createNewPage(pageSize, schoolCodeCellIndex, newSheet, pageList, schoolListCache, JSZG_COLUMN_IN_COLUMN_SIZE,evenPage);
+			createNewPage(pageSize, schoolCodeCellIndex, newSheet, pageList, schoolListCache, columnInColumnSize,evenPage);
 		}
 	}
 	
+
 	private static void createNewPage(int pageSize,int schoolCodeCellIndex,SXSSFSheet newSheet,
 			List<SXSSFRow> pageList, List<Row> schoolListCache, int columnSize, boolean evenPage) {
 		pageList.clear();
@@ -253,6 +312,16 @@ public class ExcelHandlerUtil2 {
 		//创建完成新页之后，写院校代码
 		String schoolCode = schoolListCache.get(0).getCell(schoolCodeCellIndex).getStringCellValue().toString();
 		String schoolName = schoolListCache.get(0).getCell(schoolCodeCellIndex+1).getStringCellValue().toString();
+		
+		
+		
+//		Row row = schoolListCache.get(0);
+//		String schoolCode = row.getCell(0).getStringCellValue().toString()+"-" + row.getCell(2).getStringCellValue().toString();
+		/**
+		 * 针对研究生考试，考点代码还要连接上地市代码
+		 * 2021-12-10
+		 */
+		
 		pageList.get(0).getCell(0).setCellValue("考点代码：" + schoolCode);
 		pageList.get(0).getCell(3).setCellValue("考点名称：" + schoolName);
 //		copyCell(schoolListCache.get(0).getCell(schoolCodeCellIndex), pageList.get(0).getCell(1));//学校编码第1列
